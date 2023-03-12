14a13e80e397fcd5682ec43076cdac27c8836f1f
Marco Antonio Arias Antolín <marcoariasantolin@google.com>
2023-03-12 15:29:08 +0100

    Estilo del Apartado 2.1 (fondo blanco y letra negra). Scripts del Apartado 1.5.2 (coger la altura del vídeo). Títulos que faltaban.

--- ^MF0950-UF1302/html/js/script152.js
+++ ^MF0950-UF1302/html/js/script152.js
@@ -1,18 +1,34 @@
+// Cambiar de tema claro a tema oscuro.
 var boton = document.getElementById("boton");
 var html = document.documentElement;
 var iframes = document.getElementsByTagName("iframe");
-
 boton.onclick = () => {
 	if (html.classList.contains("temaOscuro")) {
-		mensaje = "CLARO";
+		msg = "CLARO";
 		boton.innerText = "Tema OSCURO";
 	}
 	else {
-		mensaje = "OSCURO";
+		msg = "OSCURO";
 		boton.innerText = "Tema CLARO";
 	}
 	Array.from(iframes).forEach(iframe => {
-		iframe.contentWindow.postMessage(mensaje, "*");
+		iframe.contentWindow.postMessage(msg, "*");
 	});
 	html.classList.toggle("temaOscuro");
+}
+
+// Pone la altura de los dos iframes.
+var iframe;
+onmessage = (event) => {
+	iframe = setIframe(event.data.origin);
+	iframe.style.height = event.data.height + 'px';
+}
+// Pone el iframe correspondiente a la página que envía el mensaje.
+function setIframe(origin) {
+	if (origin == 'Apartado152main') {
+		iframe = document.getElementById('iframe_1');
+	} else if (origin == 'Apartado152videos') {
+		iframe = document.getElementById('iframe_2');
+	}
+	return iframe;
 }
--- ^MF0950-UF1302/html/js/script152iframes.js
+++ ^MF0950-UF1302/html/js/script152iframes.js
@@ -1,4 +1,6 @@
+// Lo pongo aquí porque no tiene hoja de estilos.
 document.documentElement.style.transition = "1.6s";
+// Cambia el color de letra de la página. El fondo lo hereda.
 onmessage = (event) => {
 	if (event.data == "CLARO") {
 		document.documentElement.style.color = "black";
@@ -12,3 +14,24 @@ window.addEventListener("message", (event) => {
 	// El contenido del mensaje está en event.data
 }, false);
 */
+
+// Manda un mensaje con la altura de la página y el origen (dos páginas tienen el mismo script).
+origin = this.location.href.split('/');
+origin = origin[origin.length - 1].split('.')[0];
+var msg = {
+	origin: origin,
+	height: 0 // Se pone al mandar el mensaje.
+}
+var video = document.getElementsByTagName('video');
+if (video[0]) {
+	// Hay que esperar a que se carge el vídeo para tener la altura correcta.
+	video[0].onloadedmetadata = () => { sendMsg(); }
+} else {
+	sendMsg();
+}
+// Pone la altura y manda el mensaje.
+function sendMsg() {
+	msg.height = Math.max(document.body.scrollHeight, document.body.offsetHeight,
+		document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+	parent.postMessage(msg, '*');
+}
